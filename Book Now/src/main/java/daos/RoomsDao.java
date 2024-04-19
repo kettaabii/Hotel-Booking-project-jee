27a67a9c017fa75fe9cd1a4e1231124d120fdbf4
kettaabii <elkettabimohammedamin@gmail.com>
2024-04-19 17:31:55 +0100

    4th commit

--- Book Now/src/main/java/daos/RoomsDao.java
+++
@@ -1,9 +0,0 @@
-package daos;
-
-import modal.Rooms;
-import java.sql.SQLException;
-import java.util.List;
-
-public class RoomsDao {
-
-}
--- Book Now/src/main/java/daos/reservationDAO.java
+++
@@ -1,9 +0,0 @@
-package daos;
-import modal.Reservation;
-import java.sql.SQLException;
-import java.util.List;
-
-
-public class reservationDAO {
-
-}
